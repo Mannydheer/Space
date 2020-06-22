0556bf27e3feabb6364c1c6578e479ffc4d4dcc2
Manny Dheer <mannydheer@gmail.com>
2020-06-22 18:18:49 -0400

    fix: typing exports

--- server/models/spaceModel.ts
+++ server/models/spaceModel.ts
@@ -1,6 +1,6 @@
-import { Typings } from "../typings";
+import { RemoteSpaceData } from "../typings/SpaceData";
 
-export const spaceModel = (data: Typings.RemoteSpaceData) => {
+export const spaceModel = (data: RemoteSpaceData) => {
   return {
     lon: data.iss_position.longitude,
     lat: data.iss_position.latitude,
